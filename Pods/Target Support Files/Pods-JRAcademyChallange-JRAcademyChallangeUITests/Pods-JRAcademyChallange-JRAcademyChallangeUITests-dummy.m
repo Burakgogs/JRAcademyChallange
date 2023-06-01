f8b985e09e07ef4eddfe78b3fbb165431e645d03
Burak GÖĞÜŞ <burak.gogus@halkbank.com.tr>
2023-06-02 00:03:26 +0300

    first push + carbon + swiftlint

---
+++ Pods/Target Support Files/Pods-JRAcademyChallange-JRAcademyChallangeUITests/Pods-JRAcademyChallange-JRAcademyChallangeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JRAcademyChallange_JRAcademyChallangeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_JRAcademyChallange_JRAcademyChallangeUITests
+@end
